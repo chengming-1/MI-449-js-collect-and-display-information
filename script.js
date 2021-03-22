5c084fa76e1366eca756aa874e67481fe906bd91
chengming Wang <wangc115@msu.edu>
2021-03-22 17:01:30 -0400

    first

---
+++ script.js
@@ -0,0 +1,30 @@
+const firstnameInput = document.getElementById('firstname')
+const lastnameInput = document.getElementById('lastname')
+const descriptionInput = document.getElementById('description')
+const emailInput = document.getElementById('email')
+const phoneInput = document.getElementById('phone')
+const preview = document.getElementById('preview')
+const rawHtml = document.getElementById('raw')
+
+const profile = function () {
+  const firstname = firstnameInput.value
+  const lastname = lastnameInput.value
+  const description = descriptionInput.value
+  const EMAIL_ADDRESS = emailInput.value
+  const PHONE_NUMBER = phoneInput.value
+
+  preview.innerHTML =
+    ' Hi, my name is ' + '<em>' + firstname + ' ' + lastname + '</em>.\n' +
+    '<p>' + description + '</p>\n' + '<p>' +
+    ' If you\'re interested in a date, you can email me at ' +
+    '<a href="mailto:EMAIL_ADDRESS" target="_blank">' + EMAIL_ADDRESS + '</a>\n' +
+    ' or give me a call at ' +
+    '<a href="tel:PHONE_NUMBER" target="_blank">' + PHONE_NUMBER + '</a>.\n' +
+    '</p>'
+  rawHtml.textContent = preview.innerHTML
+}
+firstnameInput.addEventListener('input', profile)
+lastnameInput.addEventListener('input', profile)
+descriptionInput.addEventListener('input', profile)
+emailInput.addEventListener('input', profile)
+phoneInput.addEventListener('input', profile)
